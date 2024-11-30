54b875c47a471b9fd23656b99d2b999970050ac8
MumbisDev <33399197+MumbisDev@users.noreply.github.com>
2024-11-29 20:54:11 -0500

    samples

--- backend/db/seeders/20241105231005-demo-spot-image.js
+++ backend/db/seeders/20241105231005-demo-spot-image.js
@@ -11,7 +11,7 @@ module.exports = {
     await queryInterface.bulkInsert(options, [
       {
         spotId: 1,
-        url: "https://example.com/images/spot1_image1.jpg",
+        url: "https://res.cloudinary.com/g5-assets-cld/image/upload/x_0,y_80,h_720,w_1200,c_crop/q_auto,f_auto,fl_lossy,g_center,h_1200,w_2000/g5/g5-c-60nq3bvuk-r-e-carroll-management-company-single-domain/g5-cl-1oimajv4to-ari-r-e-carroll-management-company-single-domain-greensboro-nc/uploads/2L3A0189_b_atgaid.jpg",
         preview: true,
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -32,7 +32,7 @@ module.exports = {
       },
       {
         spotId: 2,
-        url: "https://example.com/images/spot2_image2.jpg",
+        url: "https://lifeonshadylane.com/wp-content/uploads/2017/04/7f5af549fc6a2a630cf6367256319a7d.jpg",
         preview: false,
         createdAt: new Date(),
         updatedAt: new Date(),
